4ec28ddcadcbd33af6dc3e4ae5bd93fee63b3f39
corsfrancken <corsfr@gmail.com>
2020-04-14 22:02:57 +0200

    Hele upload

---
+++ kwartje.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var person = prompt("Please enter your name to claim beer", "Harry Potter");
+    if (person != Marco Borsato) {
+        document.getElementsById("button").innerHTML =
+            "Claimed";
+    }
+}
